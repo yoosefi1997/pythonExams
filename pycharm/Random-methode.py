21bbf09fff6a93fc8b9e627618f6dbd3ef28078e
Aria2ber <pitresearch.info@gmail.com>
2026-01-28 00:28:23 +0330

    updated the Random-methods.p(check the README.md file to survay this changes).

---
+++ pycharm/Random-methode.py
@@ -0,0 +1,59 @@
+# import random
+# random.randint()
+# random.choice()
+
+# from random import random, seed
+# # when we need amount of
+# seed(5)
+# print(random())
+# print(random())
+# print(random())
+# print(random())
+# print(random())
+# print(random())
+# ---------------
+
+
+from random import random, seed
+
+from decorator import append
+
+# for i in range(10):
+#     print( 5 + (random() * (10 - 5)))
+#     ----------
+# for _ in range(10):
+#     print( 5 + ( random() * (11 - 5)))
+#     -------------
+# for _ in range(12):
+#     print(int(5 + ( random() * (15 - 5))))
+# ---------
+# rand_list = []
+# for _ in range(12):
+#     print(int(5 + ( random() * (20 - 5))))
+# print("-"* 10)
+# randlist = []
+# for _ in range(12):
+#     randlist.append(int(5 + (random() * (10 - 5))))
+# print(randlist, end="\n")
+# ----------------
+# rand_list = []
+# for _ in range(10):
+#     rand_list.append(int(5 + (random() * (10 - 5))))
+# print(rand_list)
+# randset = set(rand_list)
+# print(randset)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
